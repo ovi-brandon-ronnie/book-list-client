09af7e471c0d730a30c9d9668fcd2235143f6614
Brandon Buchholz <blue.buchholz@gmail.com>
2018-01-15 17:19:30 -0800

    fixed errors with pages loading, all pages are displaying correct info, ready to redploy toserver

--- scripts/views/new-book-view.js
+++ scripts/views/new-book-view.js
@@ -1,23 +1,22 @@
 $('#newBookForm').on('submit', (e) => {
-    e.preventDefault();
-    var title = $('#title').val();
-    var author = $('#author').val();
-    var isbn = $('#isbn').val();
-    var imgUrl = $('#imageUrl').val();
-    var description = $('#description').val();
+  e.preventDefault();
+  var title = $('#title').val();
+  var author = $('#author').val();
+  var isbn = $('#isbn').val();
+  var imgUrl = $('#imageUrl').val();
+  var description = $('#description').val();
 
-    let book = new app.Book (title, author, isbn, imgUrl, description);
-    app.Book.create(book)
+  let book = new app.Book (title, author, isbn, imgUrl, description);
+  app.Book.create(book)
     .then( () => {
-        clearFields();
+      clearFields();
     });
 });
 
-
 function clearFields() {
-    $('#title').val("");
-    $('#author').val("");
-    $('#isbn').val("");
-    $('#imgUrl').val("");
-    $('#description').val("");
+  $('#title').val("");
+  $('#author').val("");
+  $('#isbn').val("");
+  $('#imgUrl').val("");
+  $('#description').val("");
 }
